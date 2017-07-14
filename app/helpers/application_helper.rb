c73d470506c21d3ead234b7c791d19fb539d7ddb
cglotr <cglotrr2@gmail.com>
2017-07-14 17:37:43 +0800

    add dynamic title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title(page_title = "")
+    base_title = "Mary Jardin App"
+    unless page_title.empty?
+      page_title + " | " + base_title
+    else
+      base_title
+    end
+  end
 end
