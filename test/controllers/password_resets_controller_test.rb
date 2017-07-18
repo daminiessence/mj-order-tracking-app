c56293751f46f1d9d6b8cdbd1f70f0232cc6ad0e
cglotr <cglotrr2@gmail.com>
2017-07-18 18:34:09 +0800

    add test files

--- test/controllers/password_resets_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class PasswordResetsControllerTest < ActionDispatch::IntegrationTest
-
-  test "should get new" do
-    get new_password_reset_path
-    assert_response :success
-  end
-end
